6897e58cb2ebc31bec74cc16da304a9350aa8e98
sharun <sharun.msgs@gmail.com>
2019-05-14 14:49:53 +0530

    added options to mandal evo script for shell colors, no shell colors and verbose output
    no colors output used for graphing
    while verbose output used for mistake detection

--- mandal_evolution.py
+++ mandal_evolution.py
@@ -1,15 +1,45 @@
 import sys, difflib
 import pandas as p
+# arg1 - mandal name or partial mandal name
+# arg2 - numeric print numeric summary without shell colors
+#		 v -  verbose output
+#		 else print numeric summary with shell colors
+# arg3 - list of files that need to be compared eg year1.csv year2.csv year3.csv
 
 df=[]
-for i in sys.argv[2:]:
+for i in sys.argv[3:]:
 	df.append(p.read_csv(i))
 
-districtfilter = lambda x: x.mandal.str.startswith(sys.argv[1])
+mandalfilter = lambda x: x.mandal.str.lower().str.startswith(sys.argv[1].lower())
 
+mandal_name = df[0][mandalfilter(df[0])].mandal.unique()[0]
 mandals_year = []
 for i in df:
-	mandals_year.append(i[districtfilter(i)].area.sort_values().to_list())
+	areas = i[mandalfilter(i)].area.str.title().sort_values().to_list()
+	mandals_year.append(areas)
+
+if sys.argv[2].isdigit():
+	tmp=mandal_name+" " + str(len(mandals_year[0]))
+else:
+	tmp=mandal_name+" \033[1;32;60m"+str(len(mandals_year[0]))
+
+for i in range(0, len(mandals_year)-1):
+	if len(mandals_year[i]) <= len(mandals_year[i+1]):
+		if sys.argv[2].isdigit():
+			tmp = tmp+" "+str(len(mandals_year[i+1]))
+		else:
+			tmp = tmp+"\x1b[1;32;60m->"+str(len(mandals_year[i+1]))
+	else:
+		if sys.argv[2].isdigit():
+			tmp = tmp+" "+str(len(mandals_year[i+1]))
+		else:
+			tmp = tmp+"\x1b[1;31;60m->"+str(len(mandals_year[i+1]))
+
+if sys.argv[2].isdigit():
+	print(tmp)
+else:
+	print(tmp+"\033[0m")
+
 
 for i in range(0,len(mandals_year)-1):
 	noc, splitm, delm, newm=[],[],[],[]
@@ -18,17 +48,25 @@ for i in range(0,len(mandals_year)-1):
 	noc = a.intersection(b)
 	allsimilar = []
 	for j in a.difference(b):
-	     similar = difflib.get_close_matches(j, b)
-	     if len(similar) == 0:
-	     	delm.append(j)
-	     else:
-	     	splitm.append((j,similar))
-	     	allsimilar.extend(similar)
+		#print(j, b) # BUG if b contains nan, emptyspace
+		similar = difflib.get_close_matches(j, b)
+		if len(similar) == 0:
+			delm.append(j)
+		else:
+			splitm.append((j,similar))
+			allsimilar.extend(similar)
 	newm=b.difference(a).difference(allsimilar)
-	print('no change', len(noc), noc)
-	print()
-	print('removed', len(delm), delm)
-	print()
-	print('added', len(newm), newm)
-	print()
-	print('division or mistakes', len(splitm), splitm )
+	if sys.argv[2].isdigit():
+		tmp='' #noop 
+	elif sys.argv[2] == 'v':
+		print('=' + str(len(noc)), '\x1b[1;31;60m-'+str(len(delm)),
+		'\x1b[0m\x1b[1;32;60m+'+ str(len(newm)),
+		'\x1b[0m\x1b[1;36;60m?'+ str(len(splitm))+'\x1b[0m')
+	else:
+		print('no change', len(noc), noc)
+		print()
+		print('removed', len(delm), delm)
+		print()
+		print('added', len(newm), newm)
+		print()
+		print('division or mistakes', len(splitm), splitm )
